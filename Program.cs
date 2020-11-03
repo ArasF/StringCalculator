fbfe3e6744fcc97381c5c915d2b5d0b6cebae2fb
ArasF <aliarasfirat@gmail.com>
2020-11-03 22:56:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace Calculator2
+{
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+          Console.WriteLine("İslemi giriniz");
+          string islem = Console.ReadLine();
+            
+            string deger = new DataTable().Compute(islem, null).ToString();
+            Console.WriteLine(deger);
+        }
+    }
+}
